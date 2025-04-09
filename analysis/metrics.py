ed96ba659dd40a886ecb238e70eee5c30597bd97
Chia-Lun Tsai <chialun2@porter.cs.illinois.edu>
2025-04-09 10:03:35 -0500

    naive cooley tukey

--- analysis/metrics.py
+++ analysis/metrics.py
@@ -5,7 +5,7 @@ def geometric_mean(times):
 
 def estimate_flops(n, time_sec):
     # 5 * N * log2(N) is a rough estimate
-    # ops = 5 * n * np.log2(n)
+    ops = 5 * n * np.log2(n)
 
     # 1.angle = 2π * k * n / N → 2 multiplies + 1 divide = 3 FLOPs
     # 2.std::polar() (i.e., cos/sin) → approx 25 FLOPs
@@ -15,5 +15,5 @@ def estimate_flops(n, time_sec):
     # 4.Complex add (sum += ...) → 2 FLOPs
 
 # Total ≈ 34 FLOPs per (k, n) pair
-    ops = 34 * (n ** 2) # CPU naive
+    # ops = 34 * (n ** 2) # CPU naive
     return ops / time_sec
--- cpp/fft_cpu.cpp
+++ cpp/fft_cpu.cpp
@@ -20,6 +20,48 @@ void fft_naive(size_t N, std::vector<std::complex<float>> &data, std::vector<std
     }
 }
 
+size_t bit_reverse(size_t x, int log2n)
+{
+    size_t result = 0;
+    for (int i = 0; i < log2n; i++)
+    {
+        result <<= 1;
+        result |= (x & 1);
+        x >>= 1;
+    }
+    return result;
+}
+
+void fft_cooley_tukey(size_t N, std::vector<std::complex<float>> &data, std::vector<std::complex<float>> &out)
+{
+    // permute input to bit reversal
+    for (size_t i = 0; i < N; i++)
+    {
+        out[i] = data[bit_reverse(i, log2(N))];
+    }
+    // cooley-tukey algorithm
+    for (int len = 2; len <= N; len <<= 1)
+    {
+        int half = len / 2;
+        for (int start = 0; start < N; start += len)
+        {
+            // do [start, start+len)
+            float angle = 2.0f * M_PI / len;
+            std::complex<float> w_step = std::polar(1.0f, -angle);
+            std::complex<float> w = std::polar(1.0f, 0.0f);
+            for (int i = 0; i < len / 2; i++)
+            {
+                // do start+i & start+i+half
+                std::complex<float> a = out[start + i];
+                std::complex<float> b = out[start + i + half];
+                out[start + i] = a + w * b;
+                out[start + i + half] = a - w * b;
+                w *= w_step;
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 2)
@@ -34,9 +76,14 @@ int main(int argc, char *argv[])
     std::vector<std::complex<float>> out(N);
 
     auto start = std::chrono::high_resolution_clock::now();
-    fft_naive(N, data, out);
+    fft_cooley_tukey(N, data, out);
     auto end = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<float> diff = end - start;
     std::cout << diff.count() << std::endl;
+
+    std::string out_file = "data/output_fft_cpu_" + std::to_string(N) + ".txt";
+    write_complex_data(out, out_file);
+
+    return 0;
 }
